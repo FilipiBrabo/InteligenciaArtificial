8a99c1b64baa35acba52f032dad286b01d90c870
filipibrabo <filipi.brabo@gmail.com>
2018-11-16 00:09:00 +0000

    Verificar se BFS funciona

--- restaUm/restaUm.py
+++ restaUm/restaUm.py
@@ -1,14 +1,26 @@
 from copy import copy, deepcopy
 
+n = 0
 # # BFS
-def buscaLargura(estados):
+def buscaLargura(estados, n):
     estados_new = []
     for s in estados:
         s_new = [resultado(s, a) for a in acoes(s)]
         estados_new = estados_new + s_new
-    if any(atingiuObj(s) for s in estados_new)
-        return solucao(estados_new)
-    return buscaLargura(estados_new)
+    for estado in estados_new:
+        n = n+1
+        print(n)
+        if atingiuObj(estado):
+            return estado
+        printEstado(estado)
+        print("#############")
+    return buscaLargura(estados_new, n)
+
+#Retorna o estado obj de ma lista de estados
+def solucao(estados):
+    for s in estados:
+        if atingiuObj(s):
+            return s
 
 # Gera um novo estado a partir de uma ação a
 def resultado(s, a):
@@ -16,13 +28,13 @@ def resultado(s, a):
    pos, acao = a
    i, j = pos
    s_new[i][j] = 'X'
-   if movimentos[acao] == 'up':
+   if acao == 'up':
        s_new[i-1][j] = 'X'
        s_new[i-2][j] = 'O'
-   elif movimentos[acao] == 'left':
+   elif acao == 'left':
        s_new[i][j-1] = 'X'
        s_new[i][j-2] = 'O'
-   elif movimentos[acao] == 'down':
+   elif acao == 'down':
        s_new[i+1][j] = 'X'
        s_new[i+2][j] = 'O'
    else:
@@ -33,14 +45,14 @@ def resultado(s, a):
 movimentos = {0:'left', 1: 'down', 2:'direita', 3:'up'}
 
 # Retorna todas as ações possíveis de um estado s
-#def acoes(s):
-#    acoes = []
-#    for i in range(len(s)):
-#        for j in range(len(s[i])):
-#            pos = (i, j)
-#            for a in acoesFactiveis(s, pos): 
-#                acoes.append(a)
-#    return acoes
+def acoes(s):
+    acoes = []
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            pos = (i, j)
+            for a in acoesFactiveis(s, pos): 
+                acoes.append(a)
+    return acoes
 
 # Retorna acoes factveis para uma posicao pos em um dado estado s
 def acoesFactiveis(s, pos):
@@ -50,24 +62,24 @@ def acoesFactiveis(s, pos):
         return acoes
     for m, n in movimentos.items():
         # Left
-        if m == 0 and j-2 < len(s):
+        if m == 0 and j-2 > -1:
             if s[i][j-1] == 'O' and s[i][j-2] == 'X':
-                a = (pos, m)
+                a = (pos, n)
                 acoes.append(a)
         # Down
         elif m == 1 and i+2 < len(s):
             if s[i+1][j] == 'O' and s[i+2][j] == 'X':
-                a = (pos, m)
+                a = (pos, n)
                 acoes.append(a)
         # Right
-        elif m == 2 and j+2 < len(s):
+        elif m == 2 and j+2 < len(s[0]):
             if s[i][j+1] == 'O' and s[i][j+2] == 'X':
-                a = (pos, m)
+                a = (pos, n)
                 acoes.append(a)
         # Up
-        elif m == 3 and i-2 < len(s):
+        elif m == 3 and i-2 > -1:
             if s[i-1][j] == 'O' and s[i-2][j] == 'X':
-                a = (pos, m)
+                a = (pos, n)
                 acoes.append(a)    
     return acoes        
                    
@@ -102,12 +114,27 @@ def main():
           ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
           ['-', '-', 'O', 'O', 'O', '-', '-'],
           ['-', '-', 'O', 'O', 'O', '-', '-']]
+          
+    s1 = [['-', 'O', '-'],
+          ['-', 'O', '-'],
+          ['O', 'O', 'O'],
+          ['O', 'X', 'O'],
+          ['O', 'O', 'O'],
+          ['-', 'O', '-'],
+          ['-', 'O', '-']]
     
+    s = []
+    s.append(s0)
+    for a in s:
+        printEstado(a)
     #printEstado(s1)
-    #printEstado(s0)
+    
+    #print(acoes(r))
     # print(atingiuObj(s0))
-       
-
+    #print(buscaLargura(s0)) 
+    #print(len(s0))
+    result = buscaLargura(s, n)
+    print(result)
+    printEstado(result)
 main()
-
-    print(bfs(s0))
+    
